0b61b9f1a3e07ee43e869c697aff75e3a8da7e96
Aaron Zemetres <azemetre@gmail.com>
2015-05-27 03:40:00 -0400

    home function now returns a 303 for location URLs. user function updated to reddit account values

--- router.js
+++ router.js
@@ -1,59 +1,73 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require("querystring");
 
 var commonHeaders = {'Content-Type': 'text/html'};
 
-// hande HTTP route GET / and POST / ie Home
+//Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
-    // if url == "/" && GET
-    if (request.url === "/") {
-        // show search
-        response.writeHead(200, commonHeaders);
+    //if url == "/" && GET
+    if(request.url === "/") {
+        if(request.method.toLowerCase() === "get") {
+        //show search
+        response.writeHead(200, commonHeaders);  
         renderer.view("header", {}, response);
         renderer.view("search", {}, response);
         renderer.view("footer", {}, response);
         response.end();
+    } else {
+        //if url == "/" && POST
+      
+        //get the post data from body
+        request.on("data", function(postBody) {
+            //extract the username
+            var query = querystring.parse(postBody.toString());
+            //redirect to /:username
+            response.writeHead(303, {"Location": "/" + query.username });
+            response.end();
+        });
+      
+        }
     }
-    // if url == "/" && POST
-        // redirect to /:username
+  
 }
 
-// handle HTTP route GET /:username
+//Handle HTTP route GET /:username
 function user(request, response) {
-    // if url == "/...."
+    //if url == "/...."
     var username = request.url.replace("/", "");
-    if (username.length > 0) {
-        response.writeHead(200, commonHeaders);
-        renderer.view("header", {}, response);
-
-        // get JSON from Treehouse
-        var studentProfile = new Profile(username);
-        // on "end"
-        studentProfile.on("end", function(profileJSON){
-            // show profile
-
-            // store the values which we need
+    if(username.length > 0) {
+        response.writeHead(200, commonHeaders);  
+        renderer.view("header", {}, response);    
+    
+        //get json from Treehouse
+        var redditProfile = new Profile(username);
+        //on "end"
+        redditProfile.on("end", function(profileJSON){
+            //show profile
+            var d = new Date((profileJSON.data.created_utc)*1000);
+            //Store the values which we need
             var values = {
-                avatarUrl: profileJSON.gravatar_url,
-                username: profileJSON.profile_name,
-                badges: profileJSON.badges.length,
-                javascriptPoints: profileJSON.points.JavaScript
+                username: profileJSON.data.name,
+                dateCreated: d.toDateString(),
+                linkKarma: profileJSON.data.link_karma,
+                commentKarma: profileJSON.data.comment_karma
             };
-            // simple response
+            //Simple response
             renderer.view("profile", values, response);
             renderer.view("footer", {}, response);
             response.end();
         });
         
-        // on "error"     
-        studentProfile.on("error", function(error){
-            // show error
+        //on "error"
+        redditProfile.on("error", function(error){
+            //show error
             renderer.view("error", {errorMessage: error.message}, response);
             renderer.view("search", {}, response);
             renderer.view("footer", {}, response);
             response.end();
         });
-
+      
     }
 }
 
