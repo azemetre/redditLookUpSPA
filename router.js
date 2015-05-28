3a8b98245561fdf7c4ac6d3b06ca9fd6bc06af11
Aaron Zemetres <azemetre@gmail.com>
2015-05-28 03:41:09 -0400

    Fixed comment stylings

--- router.js
+++ router.js
@@ -4,9 +4,9 @@ var querystring = require("querystring");
 
 var commonHeaders = {'Content-Type': 'text/html'};
 
-//Handle HTTP route GET / and POST / i.e. Home
+// handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
-    //if url == "/" && GET
+    // if url == "/" && GET
     if(request.url === "/") {
         if(request.method.toLowerCase() === "get") {
         //show search
@@ -16,13 +16,11 @@ function home(request, response) {
         renderer.view("footer", {}, response);
         response.end();
     } else {
-        //if url == "/" && POST
-      
-        //get the post data from body
+        // get the post data from body
         request.on("data", function(postBody) {
-            //extract the username
+            // extract the username
             var query = querystring.parse(postBody.toString());
-            //redirect to /:username
+            // redirect to /:username
             response.writeHead(303, {"Location": "/" + query.username });
             response.end();
         });
@@ -32,42 +30,40 @@ function home(request, response) {
   
 }
 
-//Handle HTTP route GET /:username
+// handle HTTP route GET /:username
 function user(request, response) {
-    //if url == "/...."
+    // if url == "/...."
     var username = request.url.replace("/", "");
     if(username.length > 0) {
         response.writeHead(200, commonHeaders);  
         renderer.view("header", {}, response);    
-    
-        //get json from Treehouse
+        // get json from reddit
         var redditProfile = new Profile(username);
-        //on "end"
+        // on "end"
         redditProfile.on("end", function(profileJSON){
-            //show profile
+            // store username's utc epoch creation date
             var d = new Date((profileJSON.data.created_utc)*1000);
-            //Store the values which we need
+            // store the values which we need
             var values = {
                 username: profileJSON.data.name,
+                account: profileJSON.data.name,
                 dateCreated: d.toDateString(),
                 linkKarma: profileJSON.data.link_karma,
                 commentKarma: profileJSON.data.comment_karma
             };
-            //Simple response
+            // simple response
             renderer.view("profile", values, response);
             renderer.view("footer", {}, response);
             response.end();
-        });
-        
-        //on "error"
+        }); 
+        // on "error"
         redditProfile.on("error", function(error){
-            //show error
+            // show error
             renderer.view("error", {errorMessage: error.message}, response);
             renderer.view("search", {}, response);
             renderer.view("footer", {}, response);
             response.end();
         });
-      
     }
 }
 
